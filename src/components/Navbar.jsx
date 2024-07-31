f6cfe4148d89fd16dea88b0d7b0a84d15c16ac05
iamNilabja <goswamineel23.com>
2024-07-31 23:41:54 +0530

    a

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <>
     <div className="header h-[60px] max-w-[100%] bg-violet-900 flex items-center justify-center ">
-        <p className='font-extrabold text-white'>YOUR WEATHER FORCASTER</p>
+        <p className='font-extrabold text-white mx-auto'>YOUR WEATHER FORCASTER</p>
   </div>
     
     </>
