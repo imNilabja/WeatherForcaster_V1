424bc2f0278c4597e320091c8c56fab3c708528a
iamNilabja <goswamineel23.com>
2024-07-31 21:49:34 +0530

    dynamic background feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import { useState, useEffect } from 'react'
+import { useState, useEffect,useRef } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
@@ -13,11 +13,14 @@ function App() {
   const [Maxtemp, setMaxTemp] = useState(0)
   const [Mintemp, setMinTemp] = useState(0)
   const [wspeed, setWspeed] = useState(0)
+  const [conditions, setConditions] = useState('')
 
   useEffect(() => {
     api();
   }, [])
 
+ 
+  const ref = useRef()
 
   const api = async () => {
     const req = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}?unitGroup=metric&key=HLKNA9VH887J5ENCJBR9G4ZU4&contentType=json`, { method: "GET" })
@@ -31,6 +34,7 @@ function App() {
     setMaxTemp(data.days[0].tempmax)
     setMinTemp(data.days[0].tempmin)
     setWspeed(data.days[0].windspeed)
+    setConditions(data.days[0].conditions)
   }
 
   const changeHandler = (e) => {
@@ -39,29 +43,56 @@ function App() {
 
   }
 
+  
+  const changeBg=() => {
+    if(conditions.includes("Rain")){
+      ref.current.src="/public/rainy.jpg"
+    }else if( conditions.includes("Partially cloudy")){
+ ref.current.src="/public/cloudy.jpg"
+    }
+    if(conditions.includes("Clear")){
+      ref.current.src="/public/sunny.jpg"
+    }
+    if(conditions.includes("Snow")){
+      ref.current.src="/public/snowy.jpg"
+    }
+    // if(conditions.includes("cloudy")){
+    //   ref.current.src="/public/cloudy.jpg"
+    // }
+  }
 
+  useEffect(() => {
+    changeBg()
+  }, [conditions])
   return (
     <>
-      <Navbar />
+    <div className='main-box'   >
+      <img ref={ref} src="/public/sunny.jpg" alt="" className=' flex md:w-[100%] md:h-[100%] md:mt-0 h-[50%] absolute -z-10 mt-[200px] ' />
 
-      <div className='w-[100vw] flex justify-center min-h-[200px] items-center'>
-        <input onChange={changeHandler} type="text" name='place' className='border border-black w-[250px] px-2 py-1 md:w-[300px] rounded-lg' />
-        <button className='bg-violet-300 px-3 py-1 rounded-xl mx-2' onClick={api}>submit</button>
-      </div>
-      <div className='mx-auto my-20 w-fit min-h-[200px] flex-col justify-center gap-4 items-center text-xl text-white font-bold md:block gap-y-3'>
-        
-          <p className='my-2'>Location: {location}</p>
-          <p className='my-2'>humidity: {humidity}</p>
-          <p className='my-2'>Current temperature: {temp}</p>
-          <p className='my-2'>Max temperature: {Maxtemp}</p>
-          <p className='my-2'>Min temperature: {Mintemp}</p>
-          <p className='my-2'>Wind Speed: {wspeed}</p>
+    <Navbar />
+
+<div className='w-[100vw] flex justify-center min-h-[200px] items-center'>
+  <input onChange={changeHandler} type="text" name='place' className='border border-black w-[250px] px-2 py-1 md:w-[300px] rounded-lg' />
+  <button className='bg-violet-300 px-3 py-1 rounded-xl mx-2' onClick={api}>submit</button>
+</div>
+<div className='mx-auto my-20 w-fit min-h-[200px] flex-col justify-center gap-4 items-center md:text-xl text-md text-white font-bold md:block gap-y-3'>
+  
+    <p className='my-2'>Location: {location}</p>
+    <p className='my-2'>humidity: {humidity}</p>
+    <p className='my-2'>Current temperature: {temp}</p>
+    <p className='my-2'>Max temperature: {Maxtemp}</p>
+    <p className='my-2'>Min temperature: {Mintemp}</p>
+    <p className='my-2'>Wind Speed: {wspeed}</p>
+    <p className='my-2'>Condition: {conditions}</p>
 
 
 
 
-      </div>
-      <Footer />
+</div>
+<Footer />
+
+    </div>
+    
     </>
   )
 }
